61bc4f4f93afcc4becfcbff3a11fd5e160cc43db
nilesh9593 <71830751+nilesh9593@users.noreply.github.com>
2021-03-27 21:06:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("IT WORKED");
+  res.send("Welcome to Alpha Team WebApp");
 });
 app.get("/new", function (req, res) {
-  res.send("New also worked");
+  res.send("Alpha Team Welcomes you");
 });
 
 app.listen(process.env.PORT || 5000);
